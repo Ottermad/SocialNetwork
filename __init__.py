52e39cb8c6e76d66e921c5267a1dc1179e34c7be
Ottermad <charlie.thomas@attwoodthomas.net>
2015-05-26 14:43:17 +0100

    started to merge

---
+++ __init__.py
@@ -0,0 +1,160 @@
+# Main File for Application
+
+# Import Statements
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    g,
+    flash,
+    request,
+)
+
+from flask.ext.bcrypt import (
+    check_password_hash,
+)
+
+from flask.ext.login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    login_required,
+    current_user
+)
+
+from peewee import (
+    DoesNotExist
+)
+
+import forms
+import models
+import os
+import json
+
+# Set up application - need a secret key for secure sessions
+app = Flask(__name__)
+app.secret_key = open("key.txt").readline()
+
+# Set up login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+# Login Manager Functions
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except:
+        return None
+
+# Before and After request functions
+
+@app.before_request
+def before_request():
+    """Connect to database before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each response"""
+    g.db.close()
+    return response
+
+# Routes
+
+@app.route("/")
+def index():
+    if current_user.is_authenticated():
+        return redirect(url_for("home"))
+    return render_template("index.html")
+
+@app.route("/home")
+@login_required
+def home():
+    messaging_form = forms.MessagingForm()
+    return render_template("home.html", messaging_form=messaging_form)
+
+
+@app.route("/register", methods=("POST", "GET"))
+def register():
+    form = forms.RegisterForm()
+
+    if form.validate_on_submit():
+        print("a")
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        flash("You have registered")
+        return redirect(url_for("index"))
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=("POST", "GET"))
+def login():
+    form = forms.LoginForm()
+
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except DoesNotExist:
+            flash("Your email or password does not exist.")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You've been logged in.")
+                return redirect(url_for("index"))
+            else:
+                flash("Your email or password does not exist.")
+    return render_template("login.html", form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for("index"))
+
+@app.route("/get-messages", methods=("POST", "GET"))
+@login_required
+def get_messages():
+    user = models.User.get(models.User.id == current_user.get_id())
+    other = models.User.get(models.User.username == request.form["other"])
+    json_messages = json.dumps(user.get_messages(other))
+    return json_messages
+
+@app.route("/send-message", methods=("POST", "GET"))
+@login_required
+def send_message():
+    # get form
+    form = forms.MessagingForm(request.form)
+    if form.validate():
+        # Send message
+        print("sending")
+        user = models.User.get(models.User.id == current_user.get_id())
+        result = user.send_message(form.recipient.data, form.body.data)
+        return result
+    else:
+        print(form.errors)
+        return "Validation Error."
+
+@app.route("/people", methods=("POST", "GET"))
+@login_required
+def people():
+    user = models.User.get(models.User.id == current_user.get_id())
+    json_people = json.dumps(user.get_people())
+    return json_people
+try:
+    models.initialise()
+except:
+    None
+
+
+if "HEROKU" not in os.environ:
+    if __name__ == "__main__":
+        app.run(debug=True)
