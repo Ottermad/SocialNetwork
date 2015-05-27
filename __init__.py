66d9ab9c28026926d2d6fd0cdf07b83dc159c539
Ottermad <charlie.thomas@attwoodthomas.net>
2015-05-27 12:09:07 +0100

    Added view_friend_requests route

--- __init__.py
+++ __init__.py
@@ -214,10 +214,17 @@ def user(username):
 @login_required
 def friend_request():
     username = request.form["username"]
-    print("USERNAME:",username)
+    print("USERNAME:", username)
     user = models.User.get(models.User.id == current_user.get_id())
     response = user.friend_request(username)
     return response
+
+@app.route("/view-friend-requests")
+@login_required
+def view_friend_requests():
+    user = models.User.get(models.User.id == current_user.get_id())
+    requests = user.get_friend_requests()
+    return render_template("view_friend_requests.html")
 
 try:
     models.initialise()
