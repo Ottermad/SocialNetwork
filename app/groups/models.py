103dbd26d499c331b52c65453f83eea33f7bccd6
Charles Thomas <charlie.thomas@attwoodthomas.net>
2015-11-01 12:23:07 +0000

    No idea what's going on

--- app/groups/models.py
+++ app/groups/models.py
@@ -1,1 +1,18 @@
+from peewee import Model, CharField, ForeignKeyField
+from app.models import DATABASE
 
+
+class Group(Model):
+    name = CharField(unique=True)
+    group_type = ForeignKeyField(rel_model=GroupType)
+
+
+class GroupType(Model):
+    name = CharField(unique=True)
+
+    class Meta:
+        database = DATABASE
+
+
+class UserGroups(Model):
+    pass
